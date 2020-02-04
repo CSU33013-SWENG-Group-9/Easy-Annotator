364ce637951a8fb98092ec8326aa6936be76976d
Yannick Gloster <yannickgloster@gmail.com>
2020-02-04 16:23:44 +0000

    Switch to next.js

---
+++ .next/static/webpack/static/development/pages/about.js.337c6a137c79b04a90f9.hot-update.js
@@ -0,0 +1,17 @@
+webpackHotUpdate("static\\development\\pages\\about.js",[
+/* 0 */,
+/* 1 */
+false,
+/* 2 */
+/*!*****************************************************************************************************************************************************************!*\
+  !*** multi next-client-pages-loader?page=%2Fabout&absolutePagePath=C%3A%5CUsers%5CYannick%20Gloster%5CDocuments%5CAnnotator%5CSWENG-Group-9%5Cpages%5Cabout.js ***!
+  \*****************************************************************************************************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(/*! next-client-pages-loader?page=%2Fabout&absolutePagePath=C%3A%5CUsers%5CYannick%20Gloster%5CDocuments%5CAnnotator%5CSWENG-Group-9%5Cpages%5Cabout.js! */"./node_modules/next/dist/build/webpack/loaders/next-client-pages-loader.js?page=%2Fabout&absolutePagePath=C%3A%5CUsers%5CYannick%20Gloster%5CDocuments%5CAnnotator%5CSWENG-Group-9%5Cpages%5Cabout.js!./");
+
+
+/***/ })
+])
+//# sourceMappingURL=about.js.337c6a137c79b04a90f9.hot-update.js.map
