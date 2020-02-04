c3083aa9aa1273e60002460d2662975be81084a2
Yannick Gloster <yannickgloster@gmail.com>
2020-02-04 17:57:40 +0000

    Components
    Added Components

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,9 @@
+import Layout from "../components/MyLayout";
+
 export default function About() {
   return (
-    <div>
+    <Layout>
       <p>This is the about page</p>
-    </div>
+    </Layout>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,9 @@
-import Link from "next/link";
+import Layout from "../components/MyLayout";
 
 export default function Index() {
   return (
-    <div>
-      <Link href="/about">
-        <a title="About Page">About Page</a>
-      </Link>
+    <Layout>
       <p>Hello Next.js</p>
-    </div>
+    </Layout>
   );
 }
