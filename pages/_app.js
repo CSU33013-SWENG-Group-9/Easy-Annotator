6dd0fd189cf4c4b0474af7e96ca6aac32e165e20
Yannick Gloster <yannickgloster@gmail.com>
2020-02-05 09:08:32 +0000

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import "../styles.css";
 
+// Global CSS File
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
