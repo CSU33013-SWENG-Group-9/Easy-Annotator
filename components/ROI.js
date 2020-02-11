a8abc4f9d141cfbb48bc11643c60eea794a83038
Yannick Gloster <yannickgloster@gmail.com>
2020-02-11 18:01:45 +0000

    Styled components

--- components/ROI.js
+++ components/ROI.js
@@ -1,6 +1,20 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${props => (props.primary ? "palevioletred" : "white")};
+  color: ${props => (props.primary ? "white" : "palevioletred")};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid palevioletred;
+  border-radius: 3px;
+`;
+
 class ROI extends React.Component {
   render() {
-    return <h1>Hello, {this.props.name}</h1>;
+    return <Button primary>Test</Button>;
   }
 }
 
