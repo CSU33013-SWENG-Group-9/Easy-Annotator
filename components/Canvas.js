2f043b16a1911f2246070517dfeb8ce28e355dd9
Yannick Gloster <yannickgloster@gmail.com>
2020-02-12 10:16:57 +0000

    Basic canvas functionality
    Tracking of mouse and click

--- components/Canvas.js
+++ components/Canvas.js
@@ -1,10 +1,51 @@
+const canvasTemp = {
+  margin: 40,
+  padding: 40,
+  border: "1px solid #DDDD"
+};
+
 class Canvas extends React.Component {
   constructor(props) {
     super(props);
+    this.handleMouseMove = this.handleMouseMove.bind(this);
+    this.handlePointerDown = this.handlePointerDown.bind(this);
+    this.handlePointerUp = this.handlePointerUp.bind(this);
+    this.state = { x: 0, y: 0, click: false };
+  }
+
+  handleMouseMove(event) {
+    this.setState({
+      x: event.clientX,
+      y: event.clientY
+    });
+  }
+
+  handlePointerDown() {
+    this.setState(state => ({
+      click: true
+    }));
+  }
+
+  handlePointerUp() {
+    this.setState(state => ({
+      click: false
+    }));
   }
 
   render() {
-    return <p></p>;
+    return (
+      <div
+        style={canvasTemp}
+        onMouseMove={this.handleMouseMove}
+        onPointerDown={this.handlePointerDown}
+        onPointerUp={this.handlePointerUp}
+      >
+        <p>
+          debug | mouse : ({this.state.x}, {this.state.y}){" "}
+          {this.state.click ? "Clicked" : "Unclicked"}
+        </p>
+      </div>
+    );
   }
 }
 
--- pages/third.js
+++ pages/third.js
@@ -1,10 +1,11 @@
 import Layout from "../components/MyLayout";
 import ROI from "../components/ROI";
+import Canvas from "../components/Canvas";
 
 export default function Third() {
   return (
     <Layout>
-      <ROI />
+      <Canvas />
       <p>ROI TEST PAGE</p>
     </Layout>
   );
