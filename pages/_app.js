8c3bf6c1eddb321aaa8eba19313487b9c4281571
Luke Hackett <lhackett@tcd.ie>
2020-03-02 20:43:35 +0000

    This is as much as I am willing to do

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
 import "../styles.css";
-import "../node_modules/video-react/dist/video-react.css"; // import css
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 // Global CSS File
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <Component style={{width: '100vh', height: '100vh'}} {...pageProps} />;
 }
