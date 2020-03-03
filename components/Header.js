beeaec84b5b94d2677202a5de977f952a73106b2
Yannick Gloster <yannickgloster@gmail.com>
2020-03-03 11:32:09 +0000

    Building Layout

--- components/Header.js
+++ components/Header.js
@@ -9,6 +9,9 @@ const Header = () => (
     <Link href="/">
       <a style={linkStyle}>Home</a>
     </Link>
+    <Link href="/playerlayout">
+      <a style={linkStyle}>playerLayout</a>
+    </Link>
     <Link href={"/second"}>
       <a style={linkStyle}>Second Page</a>
     </Link>
