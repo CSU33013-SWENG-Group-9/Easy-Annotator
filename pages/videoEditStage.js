54aee8b9f386a4832e2632467fed0aaf5e76f3d8
Luke Hackett <lhackett@tcd.ie>
2020-03-03 13:01:05 +0000

    Added untracked files

---
+++ pages/videoEditStage.js
@@ -0,0 +1,12 @@
+import Layout from "../components/MyLayout";
+import EditFrameHolder from "../components/editFrameHolder";
+
+export default function VideoEditStage() {
+    return (
+        <Layout style={{width: '100vh', height: '100vh'}}>
+            <EditFrameHolder/>
+        </Layout>
+    );
+}
+
+
