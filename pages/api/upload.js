c722cb8318d9d5194898cb24487d61896c7a617c
Yannick Gloster <yannickgloster@gmail.com>
2020-03-04 18:27:15 +0000

    Change naming of file

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -6,7 +6,16 @@ export const config = {
   }
 };
 
-const upload = multer({ dest: "uploads/" });
+var storage = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, "uploads");
+  },
+  filename: function(req, file, cb) {
+    cb(null, "video.mp4");
+  }
+});
+
+var upload = multer({ storage: storage });
 
 export default async (req, res) => {
   upload.single("video")(req, {}, err => {
