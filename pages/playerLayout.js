f68c44ce378e6db8a3e6e7896ec633dd4dd846c8
Yannick Gloster <yannickgloster@gmail.com>
2020-03-10 13:46:19 +0000

    Work

--- pages/playerLayout.js
+++ pages/playerLayout.js
@@ -1,6 +1,8 @@
 import { Box, Card, Image, Heading, Text, Flex, Button } from "rebass";
 import Canvas from "../components/Canvas";
 import ReactPlayer from "react-player";
+import { ThemeProvider } from "theme-ui";
+import defaultTheme from "../themes/default";
 
 const border = {
   border: "1px solid #DDD"
@@ -40,74 +42,76 @@ function VideoPlayer(props) {
 
 export default function PlayerLayout() {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        minHeight: "100vh"
-      }}
-      style={border}
-    >
+    <ThemeProvider theme={defaultTheme}>
       <Box
         sx={{
-          p: 3
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh"
         }}
         style={border}
       >
-        Header
-      </Box>
-      <Box
-        sx={{
-          flex: "1 1 auto",
-          p: 0
-        }}
-        style={border}
-      >
-        <Flex
+        <Box
           sx={{
-            flexWrap: "wrap"
+            p: 3
           }}
           style={border}
         >
-          <Box
+          Header
+        </Box>
+        <Box
+          sx={{
+            flex: "1 1 auto",
+            p: 0
+          }}
+          style={border}
+        >
+          <Flex
             sx={{
-              p: 3,
-              flexGrow: 20,
-              flexBasis: 0,
-              minWidth: 360
+              flexWrap: "wrap"
             }}
             style={border}
           >
-            <VideoPlayer videoUploaded={false} />
+            <Box
+              sx={{
+                p: 3,
+                flexGrow: 20,
+                flexBasis: 0,
+                minWidth: 360
+              }}
+              style={border}
+            >
+              <VideoPlayer videoUploaded={false} />
 
-            <ReactPlayer
-              id="react-player"
-              url="/video.mp4"
-              width="100%"
-              height="100%"
-              playing={true}
-            />
-          </Box>
-          <Box
-            sx={{
-              p: 3,
-              flexGrow: 2,
-              flexBasis: 150
-            }}
-            style={border}
-          >
-            ROIS DROP DOWN
-          </Box>
-        </Flex>
+              <ReactPlayer
+                id="react-player"
+                url="/video.mp4"
+                width="100%"
+                height="100%"
+                playing={true}
+              />
+            </Box>
+            <Box
+              sx={{
+                p: 3,
+                flexGrow: 2,
+                flexBasis: 150
+              }}
+              style={border}
+            >
+              ROIS DROP DOWN
+            </Box>
+          </Flex>
+        </Box>
+        <Box
+          sx={{
+            p: 3
+          }}
+          style={border}
+        >
+          © IBM {new Date().getFullYear()}
+        </Box>
       </Box>
-      <Box
-        sx={{
-          p: 3
-        }}
-        style={border}
-      >
-        © IBM {new Date().getFullYear()}
-      </Box>
-    </Box>
+    </ThemeProvider>
   );
 }
