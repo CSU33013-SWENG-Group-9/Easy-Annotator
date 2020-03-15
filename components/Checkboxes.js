5ae217c209934c9f1562fc0c6731dd070e402ea4
Jack Gilbride <jackgilbride999@gmail.com>
2020-03-15 16:07:27 +0000

    Working through checkboxes with comments

--- components/Checkboxes.js
+++ components/Checkboxes.js
@@ -3,8 +3,38 @@ import theme from '../themes/default';
 import ReactPlayer from 'react-player';
 import { Checkbox, FormControlLabel } from "@material-ui/core";
 
+const items = [
+    'One',
+    'Two',
+    'Three',
+  ];
+
 class Checkboxes extends React.Component {
 
+
+    createCheckboxes = () => (
+        /* Iterate over items array and create checkbox for each item*/
+        items.map(this.createCheckbox)
+    )
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-12">
+
+                        <form onSubmit={this.handleFormSubmit}>
+                            {/* Create instances of Checkbox component dynamically. */}
+                            {this.createCheckboxes()}
+                            {/* Create button of type submit which submits a form when user clicks on it. */}
+                            <button className="btn btn-default" type="submit">Save</button>
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Checkboxes;
