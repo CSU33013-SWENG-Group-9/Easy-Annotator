f742a83e3094205da146a26d3fef58ac389de96b
Jack Gilbride <jackgilbride999@gmail.com>
2020-03-21 13:27:27 +0000

    Added PageBody component.... functionality still to be added

---
+++ components/PageBody.js
@@ -0,0 +1,20 @@
+import { Box, Card, Image, Heading, Text, Flex, Button } from "rebass";
+
+import Canvas from "../components/Canvas";
+import SurgeryPlayer from "../components/SurgeryPlayer";
+
+import { ThemeProvider } from "theme-ui";
+import defaultTheme from "../themes/default";
+import VideoUploadForm from "../components/VideoUploadForm";
+import Checkboxes from "../components/Checkboxes";
+
+class PageBody extends React.Component {
+
+    render(){
+        return(
+            <div></div>
+        );
+    }
+}
+
+export default PageBody;
