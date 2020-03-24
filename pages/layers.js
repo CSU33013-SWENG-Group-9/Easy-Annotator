55464a415e72b4bac034e624ce3ba7f03ae8d3f2
Cian Galligan <ciangalligan7@gmail.com>
2020-03-24 18:05:34 +0000

    Layers functionality

--- pages/layers.js
+++ pages/layers.js
@@ -4,3 +4,30 @@ import { faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 import { faEye } from '@fortawesome/free-solid-svg-icons'
 import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
+export default function Layers(props) {
+    const {listrois, selected} = props
+    return (
+        <div style={styles.layersWrapper}>
+            {listrois && listrois.map((rois, index) => {
+                return(
+                    <div style={{...styles.layer,background: selected === index ? '#e9e9e9': 'white' }} key={index}>
+                        <div style={styles.eyeWrapper} onClick={() => {props.onEyeClick(index)}}>
+                            {rois.visible ? 
+                                <FontAwesomeIcon  onClick={() => {props.onEyeClick}} icon={faEye} style={styles.eye}/>
+                                : <FontAwesomeIcon  onClick={() => {props.onEyeClick}} icon={faEyeSlash} style={styles.eye}/>
+                            }
+                        </div>
+                        <div style={styles.textWrapper} onClick={() => {props.setSelected(index)}}>
+                            <span>{rois.label.title}</span>
+                            <span>{rois.label.type}</span>
+                        </div>
+                        <div style={styles.timeWrapper}  onClick={() => {props.setSelected(index)}}>
+                            {rois.numSeconds}
+                            <FontAwesomeIcon  onClick={() => {props.onDeleteClick(index)}} icon={faTrash} style={styles.eye}/>
+                        </div>                           
+                    </div> 
+                )
+            })}  
+        </div>
+    )
+}
