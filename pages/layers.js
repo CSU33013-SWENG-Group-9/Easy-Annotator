4aa44127c76986ba9a1dbd2f9fbb6e62bf4bd252
Cian Galligan <ciangalligan7@gmail.com>
2020-03-24 17:58:57 +0000

    Create layers.js
    new layer page and imports

---
+++ pages/layers.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEyeSlash } from '@fortawesome/free-solid-svg-icons'
+import { faEye } from '@fortawesome/free-solid-svg-icons'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+
